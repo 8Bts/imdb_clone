1aa3c2510a2defb1aea0f4124acf73d7aba9dd57
8Bts <mcmizze@yahoo.com>
2021-10-11 21:56:44 +0400

    Add Movie model and specs

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  # Association test
+  it { should has_and_belongs_to_many(:categories, join_table: "categories_movies") }
+  it { should have_many(:votes) }
+
+  # Validation tests
+  it { should validate_presence_of(:title) }
+  it { should validate_length_of(:title).is_at_least(1).is_at_most(32) }
+
+  it { should validate_presence_of(:description) }
+  it { should validate_length_of(:description).is_at_least(10).is_at_most(255) }
+
+  it { should validate_presence_of(:image) }
+  it { should allow_value('https://foo.com').for(:image) }
+  it { should_not allow_value('foo.bar').for(:image) }
+end
