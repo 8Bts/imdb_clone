f7cefaa370dd7d95a1d6005d09d465a807efaaf2
8Bts <mcmizze@yahoo.com>
2021-10-15 00:04:30 +0400

    Fix test failures for SessionController

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -12,13 +12,12 @@ RSpec.describe 'Sessions', type: :request do
   describe "POST /sign_in" do
     context 'When User exists and credentials are valid' do
       before do
-        @session_before = session[:user_id]
-        User.create('Tom', 'tom@hanks.com', '12345')
+        User.create(name: 'Tom', email: 'tom@hanks.com', password: '12345')
         post '/sign_in', params: { email: 'tom@hanks.com', password: 12345 } 
       end
 
       it 'Creates new session' do
-        expect(@session_before)).to_not eq(session[:user_id])
+        expect(session[:user_id]).to_not be_nil
       end
 
       it 'Responds with status code 201' do
@@ -32,12 +31,11 @@ RSpec.describe 'Sessions', type: :request do
 
     context 'When User doesn\'t exist or credentials are invalid' do
       before do
-        @before_session = session[:user_id]
         post '/sign_in', params: { email: 'Tom', password: '12345' } 
       end
 
       it 'Doesn\'t create new session' do
-        expect(@before_session).to eq(session[:user_id])
+        expect(session[:user_id]).to be_nil
       end
 
       it 'Responds with status code 422' do
@@ -45,15 +43,16 @@ RSpec.describe 'Sessions', type: :request do
       end
 
       it "Renders :new template for sign in form" do
-        assert_template 'session/new'
+        assert_template 'sessions/new'
       end
     end
   end
 
   describe "DELETE /log_out" do
     before do
-      session[:user_id] = 1
-      delete '/log_out' 
+      User.create(name: 'Tom', email: 'tom@hanks.com', password: '12345')
+      post '/sign_in', params: { email: 'tom@hanks.com', password: 12345 } 
+      delete '/logout' 
     end
 
     it "Empties the session" do
