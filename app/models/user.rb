4bc3ad6c9de46dd54164d8e9204594eefb98854d
8Bts <mcmizze@yahoo.com>
2021-10-16 17:02:26 +0400

    Add password validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { in: 3..12 }
   validates :admin_level, allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 3 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Invalid email format' }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'is invalid' }
+  validates :password, length: { in: 8..ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED }
 
   def default_values
     self.admin_level = 0  if self.admin_level.nil?
