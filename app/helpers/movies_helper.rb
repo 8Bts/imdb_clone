a7e035a3d9705b239454ac4764c4048f7957d9de
8Bts <mcmizze@yahoo.com>
2021-10-21 15:55:21 +0400

    Add helper methods for movies view logics

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,19 @@
 module MoviesHelper
+  def user_box
+    if Current.user
+      render partial: 'user_box', locals: { user: Current.user }
+    else
+      render inline: '<a href=<%= log_in_path %> class="sign-in-link">Sign In</a>'
+    end
+  end
+
+  def my_vote(id)
+    vote = nil
+    vote = Current.user.votes.find_by(movie_id: id) if Current.user
+    if vote
+      render partial: 'my_vote', locals: { id: id, vote: vote }
+    else
+      render partial: 'not_rated', locals: { id: id }
+    end
+  end
 end
