2f80c8ea7ddef1bf0114a9b1f7836527d9cef6d8
8Bts <mcmizze@yahoo.com>
2021-10-21 15:54:15 +0400

    Refactor models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,15 @@ class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { in: 1..64 }
   validates :description, presence: true, length: { in: 10..255 }
   validates :image, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
+
+  def rating
+    sum = votes.sum(:rating)
+    votes_count = votes.count
+
+    if votes_count.positive?
+      (sum / votes_count).ceil(1)
+    else
+      0
+    end
+  end
 end
