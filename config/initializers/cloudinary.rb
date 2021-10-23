f510f0626dc602d375ea2a8ba0690a4f3a8ff03e
8Bts <mcmizze@yahoo.com>
2021-10-23 23:45:26 +0400

    Add carriervawe and cloudinary services

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'strawberry-cloud'
+  config.api_key = '537585132569355'
+  config.api_secret = 'N7ul2WmfS_KcCQStElL6OdJ-5Vc'
+  config.secure = true
+  config.cdn_subdomain = true
+end
