c7141ae113658b7c7b9784694801a0fc24382cfd
8Bts <mcmizze@yahoo.com>
2021-10-23 23:52:04 +0400

    Refactor Movie model to use uploader

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   has_many :votes
   has_and_belongs_to_many :categories
 
+  mount_uploader :image, MoviePosterUploader
+
   validates :title, presence: true, uniqueness: true, length: { in: 1..64 }
   validates :description, presence: true, length: { in: 10..255 }
   validates :image, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp }
@@ -9,9 +11,12 @@ class Movie < ApplicationRecord
   def rating
     sum = votes.sum(:rating)
     votes_count = votes.count
+    result = nil
 
     if votes_count.positive?
-      (sum / votes_count).ceil(1)
+      result = (sum.to_f / votes_count).ceil(1)
+      return result if result != 10
+      10 
     else
       0
     end
